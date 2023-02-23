601f81784be31555a52ee87e7b8dc899772fe50b
kekcardinal <103967680+kekcardinal@users.noreply.github.com>
2023-02-23 15:55:01 -0500

    Changements categorie . php

--- category.php
+++ category.php
@@ -1,30 +1,24 @@
 
-
-
 <?php get_header() ?>
-<main>
-    <code>category.php</code>
-    <section class="blocflex">
-<?php 
-if (have_posts()):
-    while (have_posts()): the_post();?>
-    <article>
-        <?php
-        //titre
-        
-        // the_permalink(); ?>
-        <!-- //contenu -->
-        <!-- the_content(); -->
-
-        <h1><a href="<?php the_permalink(); ?>"><?= get_the_title();?></a></h1><?php
-        //résumé
-        the_excerpt();
-
-        //trim quoi? un excerpt, 4 mots
-        ?><p><?php echo wp_trim_words(get_the_excerpt(), 10 );?></p><?php
-        ?></article><?php
-    endwhile;
-endif;
-?></section>
+<main class="site__main">
+   <section class="blocflex">
+      <?php
+      $category = get_queried_object();
+      $args = array(
+         'category_name' => $category->slug,
+         'orderby' => 'title',
+         'order' => 'ASC'
+      );
+      $query = new WP_Query( $args );
+      if ( $query->have_posts() ) :
+         while ( $query->have_posts() ) : $query->the_post(); ?>
+            <article>
+               <h2><a href="<?php the_permalink(); ?>"> <?= get_the_title(); ?></a></h2>
+               <p><?= wp_trim_words(get_the_excerpt(), 15) ?></p>
+            </article>
+         <?php endwhile; ?>
+      <?php endif;
+      wp_reset_postdata();?>
+   </section>
 </main>
 <?php get_footer(); ?>
