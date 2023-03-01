ce48006216e89c89c41b8b43f9612ad0a178adf6
kekcardinal <103967680+kekcardinal@users.noreply.github.com>
2023-03-01 12:11:54 -0500

    C9 - modification query pour recherche categorie

--- header.php
+++ header.php
@@ -36,10 +36,16 @@
         <h3>menu Secondaire</h3>
         <?php 
         
-        
         $category = get_queried_object();
+        if (isset($category))
+        {
+            $lemenu = $category -> slug;
+        }else{
+            $lemenu = "31w";
+        }
+       
         wp_nav_menu(array(
-            "menu" => "cours",
+            "menu" => $lemenu,
             "container" => "nav"
         ));?>
     </aside>
