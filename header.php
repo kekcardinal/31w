5e4d1601ee1b95e89691c220aaaa2cee70ccb6a3
kekcardinal <103967680+kekcardinal@users.noreply.github.com>
2023-03-16 15:56:52 -0400

    C14 - Rollback car problème causé par menuburger

--- header.php
+++ header.php
@@ -1,43 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Wordpress - Felix B-Cousineau</title>
     <?php wp_head(); ?>
 </head>
-<body class="site <?= (is_front_page()?"no-aside":"")?>">
+
+<body class="site <?= (is_front_page()?"no-aside":""); ?>">
     <header class="site_entete">
         <section class="logomenu">
-            <?php the_custom_logo(); ?> 
-            <div class="menusearch">
-                <input type="checkbox" id="chkBurger">
-                <?php wp_nav_menu(array(
+            <?php the_custom_logo(); ?>
+            <?php wp_nav_menu(
+                array(
                     'menu' => 'entete',
                     'container' => 'nav'
-                )); ?> 
-                <?= get_search_form(); ?> 
-                <label for="chkBurger" class="burger">
-                    <img src="https://s2.svgbox.net/hero-outline.svg?ic=menu&color=000" height="20" width="20">
-                </label>
-            </div> 
+                )
+            ); ?>
+
+            <?= get_search_form(); ?>
         </section>
-        <h1>
-            <a href="
-            <?php
-            //   bloginfo('url'); 
-              ?>
-            ">
-            <?php  
-            // bloginfo('name');
-             ?>
-        </a></h1> 
-        <h2><?php bloginfo('description'); ?></h2>
+        <section class="marge_entete">
+
+            <!-- <h1><a href="<? /*php bloginfo('url'); */?>"><? /*php bloginfo('name'); */?></a></h1> -->
+
+            <!-- <h2>
+                <? /*php bloginfo('description'); */?>
+            </h2> -->
+        </section>
+
     </header>
+
     <?php 
-    //if(is_front_page() == false)
+    
+    if (!is_front_page())
     {
         get_template_part("template-parts/aside");
     }
+
     ?>
