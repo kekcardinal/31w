74a405013da2b4f674ca1657d85003eb5cdf538f
kekcardinal <103967680+kekcardinal@users.noreply.github.com>
2023-04-02 20:28:09 -0400

    C19 - Exercise 4 , ajout the_field('adresse') dans template-evenement

--- template-evenement.php
+++ template-evenement.php
@@ -12,9 +12,9 @@ if ( have_posts() ) : the_post(); ?>
 <?php the_post_thumbnail("large");?>
 <h1><?= get_the_title(); ?></h1>
 <?php the_content();?>
-<p>L'adresse de l'evénement<?php the_field('adresse'); ?></p>
-<p>La date de l'événement<?php the_field('date_event'); ?></p>
-<p>Heure de l'évènement<?php the_field('heure_event'); ?></p>    
+<p>L'adresse de l'evénement : <?php the_field('adresse'); ?></p>
+<p>La date de l'événement : <?php the_field('date_event'); ?></p>
+<p>Heure de l'évènement : <?php the_field('heure_event'); ?></p>    
 
 
 <?php endif;?>
